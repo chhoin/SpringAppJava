807335b5331dc1470adf12cd4c61b437b2bfaa8c
chhoin <heistall9@gmail.com>
2017-01-09 11:41:31 +0700

    test

--- src/main/java/com/spring/app/test/Test.java
+++ src/main/java/com/spring/app/test/Test.java
@@ -1,9 +1,28 @@
-package com.spring.app.test;
-
-import com.spring.app.utilities.MyDateUtils;
+package com.spring.app.test;import java.util.Calendar;
+import java.util.Date;
 
 public class Test {
+	
+	public static int calculation(int[] value) {
+		int result = 0;
+		for (int i = 0; i < value.length; i++) {
+			result += value[i];
+		}
+		return result;
+	}
+	
+	public static void validate(int age){  
+	     if(age<18)  
+	      throw new ArithmeticException("not valid");  
+	     else  
+	      System.out.println("welcome to vote");  
+	   }  
+	
 	public static void main(String[] args) {
-		System.out.println(MyDateUtils.getDateAtBeginningOfDay(MyDateUtils.today()));
+		/*Test.validate(113);  
+	    System.out.println("rest of the code...");  */
+		
+		Date systemTime = Calendar.getInstance().getTime();
+		System.out.println(systemTime);
 	}
 }
